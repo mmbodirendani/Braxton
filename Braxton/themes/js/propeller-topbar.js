505e1375843ac2ef0974b2004d92c7b30be10a76
Rendani <31207664+mmbodirendani@users.noreply.github.com>
2021-08-30 00:11:30 +0200

    Add files via upload

---
+++ Braxton/themes/js/propeller-topbar.js
@@ -0,0 +1,28 @@
+/*Propeller topbar close*/
+/*$(document).ready(function() {
+	"use strict";
+	$("body").addClass("propeller-topbar-body");
+	$(".close-propeller-topbar").on("click",function() {
+		$('body').addClass('hidden-propeller');
+		$('.propeller-topbar').hide();
+		$('body').removeClass('propeller-topbar-body');
+	});
+});*/
+/* On Scroll Remove Propeller Topbar */
+// $(window).on("scroll",function() {
+// 	"use strict";
+// 	var IntroScroll = 56;
+// 	var pageScroll = $(this).scrollTop();
+// 	if (pageScroll > IntroScroll) {
+// //		$('.propeller-topbar').slideUp();
+// 		$('body').removeClass('propeller-topbar-body');
+// 	} else {
+// 		if ($('body').hasClass('hidden-propeller')){
+// 			$('.propeller-topbar').slideUp();
+// 			$('body').removeClass('propeller-topbar-body');	
+// 		} else {
+// 			$('.propeller-topbar').show();
+// 			$('body').addClass('propeller-topbar-body');
+// 		}
+// 	}
+// });	
